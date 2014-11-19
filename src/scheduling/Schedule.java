04eba1270c7af6f025eab7778efa4413d7d8b26b
matthewdetrick <finkle37@gmail.com>
2014-11-19 09:34:38 -0700

    This populates the initial array for use with the alg. Only works for 15 min classes right now.

--- src/scheduling/Schedule.java
+++ src/scheduling/Schedule.java
@@ -8,22 +8,138 @@ public class Schedule
 	/*
 	 * This creates the4-dimensional array but is dynamic
 	 */
-	HashMap<Integer, List<String>> classList;
-	HashMap<Integer, HashMap<Integer, List<String>>> equipment;
-	HashMap<Integer,HashMap<Integer, HashMap<Integer, List<String>>>> times;
+	/*
+	 *HashMap<Integer, List<String>> equipment;
+	 *HashMap<Integer,HashMap<Integer, List<String>>> times;
+	*/
+	int[][][] schedule;
+	String[] equipList;
+	Class[] classes;
 	
-	public Schedule(Class[] classes)
+	public Schedule( String[] equipList, Class[] classes)
 	{
-		printClasses(sortClasses(classes));
+		this.equipList = equipList;
+		this.classes = classes;
+		sortClassesEnd();
+		int end = classes[classes.length-1].getEnd();
+		sortClassesStart();
+		int start = classes[0].getStart();
+		int time = Time.numOfRotationsNeeded(start, end, 15);
+		schedule = new int[time][classes.length][];
+		addClasses();
+		printSchedule();
+		
 	}
-	public Schedule(int equipment, Class[] classes)
+	private void addClasses()
 	{
-		classes = sortClasses(classes);
-		int numOfTimeSlots = Time.numOfRotationsNeeded(classes[0].getStart(), classes[classes.length-1].getEnd(), 15);
-		/*
-		 * times holds all of the 15 min time slices that need to be considered
-		 */
-		times =new HashMap<Integer,HashMap<Integer, HashMap<Integer, List<String>>>>(numOfTimeSlots);
+		initializeEquipLists();
+		int startTime = classes[0].getStart();
+		int[] indexes=null;
+		int time, start;
+		for(int i =0;i<classes.length;i++)
+		{
+			indexes = findEquipIndexes(classes[i]);
+			time = Time.numOfRotationsNeeded(classes[i].getStart(), classes[i].getEnd(), 15);
+			start =Time.numOfRotationsNeeded(startTime, classes[i].getStart(), 15);
+			for(int j =0;j<time;j++)
+			{
+				schedule[(start+j)][i]=indexes;
+			}
+		}
+	}
+	private void initializeEquipLists()
+	{
+		for(int i=0;i<schedule.length;i++)
+		{
+			for(int j = 0;j<schedule[i].length;j++)
+			{
+				
+				schedule[i][j]=new int[0];
+			}
+		}
+	}
+	private int[] findEquipIndexes(Class c)
+	{
+		String[] equipment = c.getEquipment();
+		int[] indexes = new int[equipment.length];
+		for(int i=0;i<equipment.length;i++)
+		{
+			int j =0;
+			while(equipment[i] != equipList[j])
+				j++;
+			indexes[i]=j;
+		}
+		return indexes;
+		
+	}
+	private void sortClassesStart()
+	{
+		int c, d, n=classes.length;
+		Class swap;
+		for (c = 0; c < ( n - 1 ); c++)
+		{
+		      for (d = 0; d < n - c - 1; d++) 
+		      {
+		        if (classes[d].getStart() > classes[d+1].getStart()) 
+		        {
+		          swap       = classes[d];
+		          classes[d]   = classes[d+1];
+		          classes[d+1] = swap;
+		        }
+		      }
+		}
+	}
+	
+	private void sortClassesEnd()
+	{
+		int c, d, n=classes.length;
+		Class swap;
+		for (c = 0; c < ( n - 1 ); c++)
+		{
+		      for (d = 0; d < n - c - 1; d++) 
+		      {
+		        if (classes[d].getEnd() > classes[d+1].getEnd()) 
+		        {
+		          swap       = classes[d];
+		          classes[d]   = classes[d+1];
+		          classes[d+1] = swap;
+		        }
+		      }
+		}
+	}
+	
+	private void printSchedule()
+	{
+		for(int i =0;i<schedule.length;i++)
+		{
+			System.out.print("[");
+			for(int j =0;j<schedule[i].length;j++)
+			{
+				System.out.print("[");
+				int[] indexes = schedule[i][j];
+				for(int k=0;k<indexes.length;k++)
+				{
+					System.out.print(indexes[k]+" ");
+				}
+				System.out.print("]");
+				
+			}
+			System.out.println("]");
+		}
+	}
+	private void printArray(int[] a)
+	{
+		for(int i =0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+	private void printArray(Object[] a)
+	{
+		for(int i =0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
 	}
 	private void printClasses(Class[] c)
 	{
@@ -35,24 +151,7 @@ public class Schedule
 	/*
 	 * Simple bubblesort to sort the classes by their start date, earliest first.
 	 */
-	private Class[] sortClasses(Class[] classes)
-	{
-		int c, d, n=classes.length;
-		Class swap;
-		for (c = 0; c < ( n - 1 ); c++)
-		{
-		      for (d = 0; d < n - c - 1; d++) 
-		      {
-		        if (classes[d].getStart() > classes[d+1].getStart()) /* For descending order use < */
-		        {
-		          swap       = classes[d];
-		          classes[d]   = classes[d+1];
-		          classes[d+1] = swap;
-		        }
-		      }
-		}
-		return classes;
-	}
+	
 		
 
 }
